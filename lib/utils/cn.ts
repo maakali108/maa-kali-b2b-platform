bfb344abf12e1048ee3baf61d84f7b72bbeaf6c1
maakali108 <maakalienterprisesb2b@gmail.com>
2026-07-13 12:40:57 +0530

    Delete lib directory

--- lib/utils/cn.ts
+++
@@ -1,9 +0,0 @@
-import { clsx, type ClassValue } from 'clsx';
-import { twMerge } from 'tailwind-merge';
-
-/**
- * Merge Tailwind class names safely (handles conflicting utility classes).
- */
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
